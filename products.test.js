e6e2f8b671dbb3fb02da5489fb5729238ebadb94
jkcon <github@xiixii.pw>
2018-11-20 16:10:01 -0600

    Modularize product test suite

--- products.test.js
+++ products.test.js
@@ -1,16 +1,25 @@
-const helper = async (done, product, category, embellishment) => {
+const createOrder = async (done, product, category, embellishment) => {
   try {
+    // select product and category
+    await page.waitForSelector(product)
     await page.click(product)
+    await page.waitForSelector(category)
     await page.screenshot({ path: 'screenshot.png' })
     await page.click(category)
+
+    // select embellishment
     await page.screenshot({ path: 'screenshot2.png' })
     await page.waitForSelector(embellishment)
     await page.screenshot({ path: 'screenshot3.png' })
     await page.click(embellishment)
+
+    // submit design
     await page.screenshot({ path: 'screenshot4.png' })
     await page.waitForSelector('[data-ui="design-submit"]')
     await page.click('[data-ui="design-submit"]')
     await page.screenshot({ path: 'screenshot5.png' })
+
+    // approve design
     await page.waitForSelector('[data-ui="design-approve"]')
     await page.click('[data-ui="design-approve"]')
     await page.waitForNavigation()
@@ -21,7 +30,7 @@ const helper = async (done, product, category, embellishment) => {
   done()
 }
 
-describe('Products', () => {
+describe('Products - Stainless', () => {
   beforeAll(async () => {
     jest.setTimeout(20000)
     await page.goto('http://yeticustomshop.com/products')
@@ -31,8 +40,7 @@ describe('Products', () => {
   //   await page.close()
   // })
 
-  it('should add 20oz Stainless, Air Force Primary ', (done) => helper(done, '[data-select="rambler-tumbler-20oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
+  it('should add 20oz Stainless, Air Force Primary ', (done) => createOrder(done, '[data-select="rambler-tumbler-20oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
 
-  it('should add 20oz Stainless, Air Force Primary ', (done) => helper(done, '[data-select="rambler-tumbler-20oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
-
+  // it('should add 30oz Stainless, Air Force Primary ', (done) => createOrder(done, '[data-select="rambler-tumbler-30oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
 })
