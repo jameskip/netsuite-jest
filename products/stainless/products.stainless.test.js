1f521537d86326a39c361e3728e1f4b5e3e5f97d
jkcon <github@xiixii.pw>
2018-11-29 14:26:25 -0600

    All systems nominal

--- products/stainless/products.stainless.test.js
+++ products/stainless/products.stainless.test.js
@@ -1,50 +1,51 @@
-'use strict'
-
 const puppeteer = require('puppeteer')
 
 let browser
 
 const createOrder = async (done, product, category, embellishment) => {
-  try {
-    browser = await puppeteer.launch()
-    let page = await browser.newPage()
-    await page.goto('http://yeticustomshop.com/products')
+  jest.setTimeout(50000)
+  let page = await browser.newPage()
+  await page.goto('http://yeticustomshop.com/products')
 
-    // select product and category
-    await page.waitForSelector(product)
-    await page.click(product)
-    await page.waitForSelector(category)
-    // await page.screenshot({ path: 'screenshot.png' })
-    await page.click(category)
+  // select product and category
+  await page.waitForSelector(product)
+  await page.click(product)
+  await page.waitForSelector(category)
+  await page.screenshot({ path: `${product}.png` })
+  await page.click(category)
 
-    // select embellishment
-    // await page.screenshot({ path: 'screenshot2.png' })
-    await page.waitForSelector(embellishment)
-    // await page.screenshot({ path: 'screenshot3.png' })
-    await page.click(embellishment)
+  // select embellishment
+  await page.screenshot({ path: `${product}-2.png` })
+  await page.waitForSelector(embellishment)
+  await page.screenshot({ path: `${product}-3.png` })
+  await page.click(embellishment)
 
-    // submit design
-    // await page.screenshot({ path: 'screenshot4.png' })
-    await page.waitForSelector('[data-ui="design-submit"]')
-    await page.click('[data-ui="design-submit"]')
-    // await page.screenshot({ path: 'screenshot5.png' })
+  // submit design
+  await page.screenshot({ path: `${product}-4.png` })
+  await page.waitForSelector('[data-ui="design-submit"]')
+  await page.click('[data-ui="design-submit"]')
+  await page.screenshot({ path: `${product}-5.png` })
 
-    // approve design
-    await page.waitForSelector('[data-ui="design-approve"]')
-    await page.click('[data-ui="design-approve"]')
-    await page.waitForNavigation()
-    // await page.screenshot({ path: 'screenshot6.png' })
+  // approve design
+  await page.waitForSelector('[data-ui="design-approve"]')
+  await page.click('[data-ui="design-approve"]')
+  await page.waitForNavigation()
+  await page.screenshot({ path: `${product}-6.png` })
 
-    await browser.close()
-    done()
-  } catch (e) {
-    console.error(e)
-  }
+  done()
 }
+
+beforeAll(async () => {
+  browser = await puppeteer.launch()
+})
+
+afterAll(async () => {
+  await browser.close()
+})
 
 test('should add 20oz Stainless, Air Force Primary ', (done) => createOrder(done, '[data-select="rambler-tumbler-20oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
 
-test('should add 30oz Stainless, Air Force Primary ', (done) => createOrder(done, '[data-select="rambler-tumbler-30oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
+// test('should add 30oz Stainless, Air Force Primary ', (done) => createOrder(done, '[data-select="rambler-tumbler-30oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
 
 // test('should add Mug Stainless, Air Force Primary ', (done) => createOrder(done, '[data-select="rambler-mug-14oz"]', '[title="Shop College Logo Custom YETI Ramblers"]', '[data-key="air-force-primary-1"]'))
 
